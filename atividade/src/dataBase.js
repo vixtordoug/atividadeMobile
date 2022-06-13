b9ab1682c4a6bf8ff3cd4fefd47cae27180e248f
vixtordoug <victordouglas999@gmail.com>
2022-06-13 15:55:29 -0300

    version 3.0

--- atividade/src/dataBase.js
+++ atividade/src/dataBase.js
@@ -33,15 +33,15 @@ export default class Db {
                                 tx.executeSql(
                                     'CREATE TABLE IS NOT EXISTS usuario(id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(20), senha VARCHAR(8))',
                                     []
-                                )
+                                );
                             }
                         }
-                    )
-                })
+                    );
+                });
             })
 
     }
-    
+
     //Alimentação da Tabela de Usuários com seus dados
     addUsuario(usuario) {
         let db;
@@ -53,30 +53,73 @@ export default class Db {
         )
             .then(DB => {
                 db = DB;
-                db.transaction((tx) {
-                    tx.executeSql(
-                        '',
-                        [],
-                        function (tx, result) {
-                            console.log('intem', result.rows.length);
-                            if (result.rows.length == 0) {
-                                tx.executeSql('DROP TABLE IF EXISTS usuario', []);
-                                tx.executeSql(
-                                    'CREATE TABLE IS NOT EXISTS usuario(id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(20), senha VARCHAR(8))',
-                                    []
-                                )
+                db.transaction((tx) => {
+                    tx.executeSql('INSERT INTO usuario (nome, senha) VALUES (?,?)',
+                        [usuario.nome_usuario, usuario.senha_usuario], (tx, results) => {
+                            if (results.rowsAffected > 0) {
+                                Alert.alert('Cadastro', 'Resgistro Inserido com sucesso');
+                            } else {
+                                Alert.alert('Erro no Cadastro');
                             }
                         }
-                    )
+                    );
                 })
             })
 
     }
 
     //Alterar informações dos usuários
-    updateUsuario()
+    updateUsuario(usuario) {
+        let db;
+        SQLite.openDatabase(
+            database_name,
+            database_version,
+            database_displayname,
+            database_size,
+        )
+            .then(DB => {
+                db = DB;
+                db.transaction((tx) => {
+                    tx.executeSql('UPDATE usuario SET nome = ? senha = ? WHERE id = ?',
+                        [
+                            cliente.nome_usuario,
+                            cliente.senha_usuario,
+                            cliente.id_usuario
+                        ], (tx, results) => {
+                            if (results.rowsAffected > 0) {
+                                Alert.alert('Alteração', 'Dados alterado com Sucesso');
+                            } else {
+                                Alert.alert('Alteração', 'Erro na alteração');
+                            }
+                        });
+                })
+            })
+    }
 
     //excluir um usuário
-    deleteUsuario()
+    deleteUsuario(usuario) {
+        let db;
+        SQLite.openDatabase(
+            database_name,
+            database_version,
+            database_displayname,
+            database_size,
+        )
+            .then(DB => {
+                db = DB;
+                db.transaction((tx) => {
+                    tx.executeSql('DELETE FROM usuario WHERE id = ?',
+                        [
+                            cliente.id_usuario
+                        ], (tx, results) => {
+                            if (results.rowsAffected > 0) {
+                                Alert.alert('Exclusão', 'Usuário excluído com Sucesso');
+                            } else {
+                                Alert.alert('Exclusão', 'Erro na exclusão');
+                            }
+                        });
+                })
+            })
+    }
 
 }
